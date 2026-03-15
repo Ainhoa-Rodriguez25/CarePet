63a45f48e3194d2f07f8fdd8ab82204a1a8a619c
Ainhoa-Rodriguez <ainhoa.rodriguez@goumh.umh.es>
2026-03-15 18:20:41 +0100

    Update JwtUtil.java

--- backend/src/main/java/com/tfg/carepet/security/JwtUtil.java
+++ backend/src/main/java/com/tfg/carepet/security/JwtUtil.java
@@ -12,16 +12,16 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-@Component
+@Component // Marca la clase como un componente de Spring
 public class JwtUtil {
 
-    @Value("${jwt.secret}")
+    @Value("${jwt.secret}") // Se utiliza para leer el valor de jwt.secret del archivo application.properties
     private String secret;
 
     @Value("${jwt.expiration}")
     private Long expiration;
 
-    // Generar clave de firma
+    // Generar clave de firma a partir del atributo secret (convertido en clave criptográfica)
     private Key getSigninKey() {
         return Keys.hmacShaKeyFor(secret.getBytes());
     }
@@ -29,15 +29,15 @@ public class JwtUtil {
     // Generar token JWT
     public String generateToken(Long userId) {
         Map<String, Object> claims = new HashMap<>();
-        claims.put("userId", userId);
+        claims.put("userId", userId); // Claims son los datos que contiene el token, se guarda el userId para identificar al usuario
 
         return Jwts.builder()
-                .setClaims(claims)
-                .setSubject(userId.toString())
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + expiration))
-                .signWith(getSigninKey(), SignatureAlgorithm.HS256)
-                .compact();
+                .setClaims(claims) // Añade los datos
+                .setSubject(userId.toString()) // Establece el sujeto del token
+                .setIssuedAt(new Date()) // Establece fecha de creacion
+                .setExpiration(new Date(System.currentTimeMillis() + expiration)) // Establece una fecha de expiracion del token
+                .signWith(getSigninKey(), SignatureAlgorithm.HS256) // Firma token con clave secreta
+                .compact(); // Genera el token final como string
     }
 
     // Validar token
